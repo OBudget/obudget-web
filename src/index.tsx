7bdc8e41a7dc61bf224ed0f4ee201f5885c4001f
Sergei Shilin <shilinshtein@gmail.com>
2022-01-22 11:35:11 -0500

    Replace react-helmet with react-helmet-async
    Bump react-chartjs-2 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
+
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </StrictMode>,
   document.getElementById("root")
